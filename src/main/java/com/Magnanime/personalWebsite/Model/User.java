44f46d8ff9911ae321ad71f4c16b1b7cded74314
Sebastian <sebastianf215@gmail.com>
2020-09-06 01:04:57 +0200

    Added basic model

---
+++ src/main/java/com/Magnanime/personalWebsite/Model/User.java
@@ -0,0 +1,22 @@
+package com.Magnanime.personalWebsite.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String userName;
+    @Column
+    private String password;
+    @Column
+    private String email;
+}
